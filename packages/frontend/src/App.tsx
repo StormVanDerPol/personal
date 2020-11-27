294ae249e54509cf657372513561afec3a6e267c
stormvanderpol <psostorm@gmail.com>
2020-11-27 04:02:24 +0100

    feat: useDraggable hook

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Draggable from "./components/common/Draggable";
 import { Desktop } from "./components/templeOs";
 
 export default function App() {
   return (
     <div>
-      <Desktop />
+      <Draggable />
+      {/* <Desktop /> */}
     </div>
   );
 }
